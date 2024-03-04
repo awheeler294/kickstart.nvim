f69af198dc2a653f993d3e7892aafd94688aaaa1
awheeler294 <awheeler294@gmail.com>
2024-03-04 15:48:54 -0700

    replace rust tools with rustaceanvim

--- lua/custom/plugins/init.lua
+++ lua/custom/plugins/init.lua
@@ -66,32 +66,12 @@ return {
 			require('remember').setup{}
 		end,
 	},
-	-- {
-	-- 	-- restore cursor position on file load
-	-- 	"ethanholz/nvim-lastplace",
-	--
-	-- 	config = function()
-	-- 		require('nvim-lastplace').setup{}
-	-- 	end,
-	-- },
 
 	{
-		-- https://github.com/simrat39/rust-tools.nvim
-		"simrat39/rust-tools.nvim",
-
-		config = function()
-			local rt = require("rust-tools")
-
-			rt.setup({
-				server = {
-					on_attach = function(_, bufnr)
-						-- Hover actions
-						vim.keymap.set("n", "<C-space>", rt.hover_actions.hover_actions, { buffer = bufnr })
-						-- Code action groups
-						-- vim.keymap.set("n", "<Leader>a", rt.code_action_group.code_action_group, { buffer = bufnr })
-					end,
-			   },
-			})
-		end,
-	},
+		-- github.com/mrcjkb/rustaceanvim
+		-- install rust-analyzer: '''rustup component add rust-analyzer'''
+	   'mrcjkb/rustaceanvim',
+	   version = '^4', -- Recommended
+	   ft = { 'rust' },
+	}
 }
